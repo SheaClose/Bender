9a96b241350b63ff799b2dc3cc10be2bfce5ae8c
SheaClose <SheaClose@gmail.com>
2016-09-14 22:11:26 -0500

    corrected http error

--- controllers/appService.js
+++ controllers/appService.js
@@ -1,6 +1,6 @@
 angular.module("app")
 .service("appService", function($http){
-  var baseUrl = "http://api.brewerydb.com/v2/";
+  var baseUrl = "https://api.brewerydb.com/v2/";
   var apiKey = "key=e6dd4ca543ecb9e65e170def16b95035"
   var counter = 1;
   this.getBeers = function(){
