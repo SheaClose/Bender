e330594d02c274123c040d973c3b26a3efd5b55a
SheaClose <SheaClose@gmail.com>
2016-09-16 13:52:25 -0500

    everything but the marker labels are now working on google map

--- js/controllers/mapCtrl.js
+++ js/controllers/mapCtrl.js
@@ -1,31 +1,33 @@
 angular.module("app")
 .controller("mapCtrl", function($scope, mapService){
 
+$scope.mapRefresh=false;
 $scope.map = {
     center: {latitude: 0, longitude: 0 }
-  , zoom: 1 };
-  $scope.marker = {
+  , zoom: 7 };
+
+  $scope.markerList=[{
     coords: {
-      latitude: 30,
-      longitude: -73,
-    }
+    latitude: 33.782182,
+    longitude: -96.797557,
+    message: "This is a test Message!"
   }
-  // $scope.markerList=[{
-  //   coords: {
-  //     latitude: 10,
-  //     longitude: -73,
-  //   }
-  // },{
-  //   coords: {
-  //     latitude: 31,
-  //     longitude: -73,
-  //   }
-  // },{
-  //   coords: {
-  //     latitude: 45,
-  //     longitude: -73,
-  //   }
-  // }]
+  , id: 2
+  },{
+    coords: {
+    latitude: 34.782182,
+    longitude: -96.797557,
+    message: "This is a test Message!"
+  }
+  , id: 3
+  },{
+    coords: {
+    latitude: 35.782182,
+    longitude: -96.797557,
+    message: "This is a test Message!"
+  }
+  , id: 4
+  }]
 
   if (navigator.geolocation) {
     function error(err){
@@ -34,8 +36,15 @@ $scope.map = {
       var userLat = pos.coords.latitude;
       var userLng = pos.coords.longitude;
       $scope.map = { center: { latitude: userLat, longitude: userLng }, zoom: 15 };
+      $scope.homeMarker = {
+        coords: {
+          latitude: userLat,
+          longitude: userLng,
+        }
+        ,id: 99
+      }
     }
-    navigator.geolocation.getCurrentPosition(success, error);
+    navigator.geolocation.getCurrentPosition(success, error)
   }
 
 
