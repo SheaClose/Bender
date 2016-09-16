519ab7e7052181c1771be7280984fd0908b252dc
SheaClose <SheaClose@gmail.com>
2016-09-16 15:26:21 -0500

    map functioning, waiting for info to load and then being turned on. Currently starting work on getting brewery information getting parsed back from brewerydb

--- js/controllers/mapCtrl.js
+++ js/controllers/mapCtrl.js
@@ -1,34 +1,34 @@
 angular.module("app")
 .controller("mapCtrl", function($scope, mapService){
-
-$scope.mapRefresh=false;
-$scope.map = {
-    center: {latitude: 0, longitude: 0 }
-  , zoom: 7 };
+  //prevents map from showing up until information is obtained.
+  $scope.mapRefresh = false;
 
   $scope.markerList=[{
     coords: {
     latitude: 33.782182,
-    longitude: -96.797557,
-    message: "This is a test Message!"
+    longitude: -96.797557
   }
   , id: 2
+  , message: "This is a test Message!"
   },{
     coords: {
     latitude: 34.782182,
-    longitude: -96.797557,
-    message: "This is a test Message!"
+    longitude: -96.797557
   }
   , id: 3
+  , message: "This is a test Message!"
   },{
     coords: {
     latitude: 35.782182,
-    longitude: -96.797557,
-    message: "This is a test Message!"
+    longitude: -96.797557
   }
   , id: 4
+  , message: "This is a test Message!"
   }]
-
+  $scope.refresh = function(){
+    $scope.mapRefresh = !$scope.mapRefresh
+    console.log($scope.mapRefresh)
+  };
   if (navigator.geolocation) {
     function error(err){
     }
@@ -43,11 +43,17 @@ $scope.map = {
         }
         ,id: 99
       }
+      $scope.getBreweryByLoc(userLat, userLng);
     }
     navigator.geolocation.getCurrentPosition(success, error)
   }
-
-
-
+///testing starts here!!!!
+  $scope.getBreweryByLoc = function (lat, long) {
+    mapService.getBreweryByLoc(lat, long).then(function(response){
+      $scope.localbreweries = response;
+      console.log(response)
+      $scope.refresh()
+    })
+  }
 
 })
--- js/services/mapServices.js
+++ js/services/mapServices.js
@@ -1,8 +1,26 @@
 angular.module("app")
 .service("mapService", function($http){
-  var beerBaseUrl = "https://api.brewerydb.com/v2/";
+//basic information for use by .getBreweryByLoc
+  var beerBaseUrl =  "http://api.brewerydb.com/v2/search/geo/point?lat=";
   var beerApiKey = "key=e6dd4ca543ecb9e65e170def16b95035"
   var beerPageCounter = 0;
 
-  
+//obtains local breweries and verifies the information prior to passing along to MapCtrl
+  this.getBreweryByLoc = function(lat, long){
+    var breweryArr = [];
+    return $http({
+        method: "GET"
+      , url: beerBaseUrl + lat + "&lng=" + long + "&" + beerApiKey
+    }).then(function(response){
+      var responseArr = response.data.data;
+      for (var i = 0; i < responseArr.length; i++){
+        console.log(responseArr[i]);
+        if(responseArr[i].status = "verified"){
+            breweryArr.push(responseArr[i])
+        }
+      }
+      return breweryArr;
+    })
+  };
+
 })
