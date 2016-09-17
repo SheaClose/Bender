ab2c0c245a852dfa9d5492966c3210cbd70c0cdc
SheaClose <SheaClose@gmail.com>
2016-09-16 19:32:42 -0500

    brewery data being parsed in, list created on left, images placed to right when first loaded (if available).

--- js/controllers/breweryHomeCtrl.js
+++ js/controllers/breweryHomeCtrl.js
@@ -1,13 +1,20 @@
 angular.module('app')
 .controller("breweryHomeCtrl", function($scope, $state, mapService, breweryHomeService){
-  console.log($state.params.bId)
+  // console.log($state.params.bId)
 
   function getBreweryInfo(arg){
     breweryHomeService.getBreweryInfo(arg).then(function(response){
-      console.log(response)
+      // console.log(response)
       $scope.beerList = response;
     })
   }
+  var getBrewery = function(id){
+    breweryHomeService.getBrewery(id).then(function(response){
+      $scope.brewery = response;
+      $scope.breweryInfo = response.images.squareLarge;
+    })
+}
 
   getBreweryInfo($state.params.bId);
+  getBrewery($state.params.bId);
 })
--- js/services/breweryHomeService.js
+++ js/services/breweryHomeService.js
@@ -1,10 +1,11 @@
 angular.module("app")
 .service("breweryHomeService", function($http){
   var beerBaseUrl =  "https://api.brewerydb.com/v2/brewery/";
-  var beerApiKey = "/beers?key=e6dd4ca543ecb9e65e170def16b95035"
+  var beerApiKey = "key=e6dd4ca543ecb9e65e170def16b95035"
+  var beersUrl = "/beers?";
 
   this.getBreweryInfo = function(id){
-    return $http.get(beerBaseUrl + id + beerApiKey).then(function(response){
+    return $http.get(beerBaseUrl + id + beersUrl + beerApiKey).then(function(response){
       var beerArr = response.data.data;
 
       var beerList = [];
@@ -13,8 +14,14 @@ angular.module("app")
           beerList.push(beerArr[i])
         }
       }
-      console.log(beerList);
+      // console.log(beerList);
       return beerList;
+    })
+  }
+  this.getBrewery = function(id){
+    return $http.get(beerBaseUrl + id + "?" + beerApiKey).then(function(response){
+      console.log(response.data.data)
+      return response.data.data;
     })
   }
 })
